e33b7144893d8e4b46431ba72e794abda842398d
Gavin Kondrath <gavin.kondrath@gmail.com>
2023-12-20 20:03:08 +0700

    housekeeping

--- config.py
+++
@@ -1,3 +0,0 @@
-import os
-
-launcher_path = os.path.dirname(os.path.abspath(__file__))
--- tests/create_youtube_short_test.py
+++
@@ -1,8 +0,0 @@
-import create_youtube_short
-import pytest
-
-# use command pytest path_to_py -v for verbose tests
-
-
-def test_get_story():
-    assert create_youtube_short.get_story_from_reddit()
