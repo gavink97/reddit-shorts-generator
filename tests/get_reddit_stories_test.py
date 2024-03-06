192d7dd426f759bdc8a6328dd29cd69a9b54eb0c
Gavin Kondrath <78187175+gavink97@users.noreply.github.com>
2024-03-06 17:57:40 +0700

    updated test

--- tests/get_reddit_stories_test.py
+++ tests/get_reddit_stories_test.py
@@ -23,9 +23,8 @@ def test_connect_to_reddit(load_credentials):
 
 
 def test_get_story_from_reddit(load_credentials):
-    test_mode = True
     reddit = None
     platform = 'youtube'
     (platform_tts_path, platform_tts) = tts_for_platform(platform)
 
-    get_story_from_reddit(subreddits, platform_tts, test_mode, reddit, platform)
+    get_story_from_reddit(subreddits, platform_tts, reddit, platform)
